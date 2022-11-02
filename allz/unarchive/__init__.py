c0167d47d819a19c76852458da055a9e77d279ba
= <yoga333@qq.com>
2022-11-02 15:28:45 +0800

    tox test update and upload test comopress file

--- allz/unarchive/__init__.py
+++ allz/unarchive/__init__.py
@@ -1,9 +1,11 @@
 from allz.unarchive.tar_bz_process import TarBzProcess
 from allz.unarchive.unar_process import UnarProcess
 from allz.unarchive.zip_process import ZipProcess
+from allz.unarchive.unarchive_main import Unarchive
 
 __all__ = [
     "TarBzProcess",
     "UnarProcess",
-    "ZipProcess"
+    "ZipProcess",
+    "Unarchive"
 ]
